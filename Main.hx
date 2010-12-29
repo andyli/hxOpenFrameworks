87b5918c5c51398e51c3593c38083df49fdb79a1
andyli <andy@onthewings.net>
2010-12-30 05:02:47 +0800

    changes to skip compiling OF before compiling ndll, added rtaudio to the source

--- Main.hx
+++ Main.hx
@@ -1,13 +1,10 @@
 package;
 
-import of.addons.ofxShader.Shader;
-
 class Main {
 	static function main():Void {
-		//cpp.vm.Gc.enable(false);
 		//uncomment one of the following lines to see the example
 		
-		of.examples.GraphicsExample.main();
+		//of.examples.GraphicsExample.main();
 		//of.examples.ImageLoaderExample.main();		//you need to copy files from OF\apps\examples\imageLoaderExample\bin\data to bin\data
 		//of.examples.TextureExample.main();
 		//of.examples.PolygonExample.main();
@@ -17,7 +14,7 @@ class Main {
 		//of.examples.MovieGrabberExample.main();
 		//of.examples.MoviePlayerExample.main();		//you need to copy files from OF\apps\examples\moviePlayerExample\bin\data to bin\data
 		//of.examples.SoundPlayerExample.main();		//you need to copy files from OF\apps\examples\soundPlayerExample\bin\data to bin\data
-		//of.examples.AudioOutputExample.main();		//not working...
+		of.examples.AudioOutputExample.main();		//not working...
 		//of.examples.SoundPlayerFFTExample.main();		//you need to copy files from OF\apps\examples\soundPlayerFFTExample\bin\data to bin\data
 		//of.examples.AdvancedEventsExample.main();
 	}
